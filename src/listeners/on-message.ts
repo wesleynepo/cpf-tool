01211b5a8bbf107ce40c0e3fedef5b8436336f34
wesleynepo <wesleynepomuceno1@gmail.com>
2022-01-24 22:12:36 -0300

    fix: empty input not inserting

--- src/listeners/on-message.ts
+++ src/listeners/on-message.ts
@@ -2,13 +2,15 @@ export const onMessage = (message: any) => {
     if(message.data) insertTextAtCursor(message.data)
 }
 
-function insertTextAtCursor(text: string) {
+const insertTextAtCursor = (text: string) => {
     let element = document.activeElement as HTMLInputElement;
     let value = element.value;
-
+    
     if (element.selectionStart && element.selectionEnd) {
         let endIndex = element.selectionEnd;
         element.value = value.slice(0, endIndex) + text + value.slice(endIndex);
         element.selectionStart = element.selectionEnd = endIndex + text.length;
+    } else {
+        element.value = text
     }
 }
